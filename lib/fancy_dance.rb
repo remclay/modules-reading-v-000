2bf85bc75e774de9358a3301d7413e587d2b7172
Remmy Clay <remmy.lauren@gmail.com>
2017-12-19 20:51:33 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -17,7 +17,6 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
-
   end
 
   module ClassMethods
@@ -25,7 +24,5 @@ module FancyDance
     def metadata
       "This class produces objects that love to dance."
     end
-    
   end
-
 end
